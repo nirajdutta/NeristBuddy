e61dbe7a38b1b466e5cd9ab45ef04f757e3d70b6
Niraj Dutta <63532862+nirajdutta@users.noreply.github.com>
2021-03-23 00:35:34 +0530

    LoginActivity.kt

--- app/src/main/java/com/diplomaproject/neristbuddy/LoginActivity.kt
+++ app/src/main/java/com/diplomaproject/neristbuddy/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.diplomaproject.neristbuddy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var btnlogin: Button
+    lateinit var etId: EditText
+    lateinit var etPassword: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnlogin = findViewById(R.id.btnLogin)
+        etId = findViewById(R.id.etId)
+        etPassword = findViewById(R.id.etPassword)
     }
 }
